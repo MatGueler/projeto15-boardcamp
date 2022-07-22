8e0bfe009640f757e12be9f4fe089b1762019a26
MattGueler <mateus.gueler@gmail.com>
2022-07-22 15:21:38 -0300

    recebe os valores de cada propriedade pelo body

--- src/Controllers/GamesController.js
+++ src/Controllers/GamesController.js
@@ -7,34 +7,42 @@ export async function getGames(req, res) {
     res.send(games)
 }
 
-// export async function postCategories(req, res) {
+export async function postGames(req, res) {
 
-//     const { name } = req.body
-//     const userSchema = joi.object({
-//         name: joi.string().required()
-//     });
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body
 
-//     const validation = userSchema.validate(name, { abortEarly: true });
 
-//     if (validation.error) {
-//         console.log(validation.error.details)
-//         return res.status(422).send('Categoria deve possuir um nome!')
-//     }
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        image: joi.required(),
+        stockTotal: joi.number().required(),
+        categoryId: joi.number().required(),
+        pricePerDay: joi.number().required()
+    });
 
-//     try {
+    const validation = userSchema.validate({ name, image, stockTotal, categoryId, pricePerDay }, { abortEarly: true });
 
-//         const valid = await connection.query(`SELECT * FROM categories WHERE name='${name}'`);
-//         if (valid.rowCount === 0) {
-//             await connection.query(`INSERT INTO categories (name) VALUES ('${name}')`);
+    if (validation.error) {
+        console.log(validation.error.details)
+        return res.status(422).send('Categoria deve possuir um nome!')
+    }
 
-//             return res.status(201).send('Usuário criado!')
-//         }
-//         else {
-//             return res.status(400).send('Essa categoria já existe!')
-//         }
-//     }
-//     catch {
-//         res.status('Deu erro!')
-//     }
-// }
+    // try {
+
+    //     const valid = await connection.query(`SELECT * FROM games`);
+    //     if (valid.rowCount === 0) {
+    //         await connection.query(`INSERT INTO categories (name) VALUES ('${name}')`);
+
+    //         return res.status(201).send('Usuário criado!')
+    //     }
+    //     else {
+    //         return res.status(400).send('Essa categoria já existe!')
+    //     }
+    // }
+    // catch {
+    //     res.status('Deu erro!')
+    // }
+
+    res.send(name)
+}
 
