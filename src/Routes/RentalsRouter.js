6287e7fe522ae190768741b2c3c0e5f60ac52e77
MattGueler <mateus.gueler@gmail.com>
2022-07-25 17:13:56 -0300

    Deleta o aluguel, sem validações

--- src/Routes/RentalsRouter.js
+++ src/Routes/RentalsRouter.js
@@ -1,9 +1,11 @@
-import { getRentals, postRentals } from '../Controllers/RentalsController.js'
+import { getRentals, postRentals, deleteRentals } from '../Controllers/RentalsController.js'
 import { Router } from 'express'
 
 const server = Router()
 
 server.get('/rentals', getRentals)
 server.post('/rentals', postRentals)
+// server.post('/rentals/:id/return', postRentals)
+server.delete('/rentals/:id', deleteRentals)
 
 export default server;
